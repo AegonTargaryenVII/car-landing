82d7e6fdd300ce3abad5abd0360b88d353b27509
guruprasaath.a <guruprasaath.a@npci.org.in>
2024-10-23 18:02:32 +0530

    Drake changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react'
 import { Background } from './Components/Background/Background';
 import { Navbar } from './Components/Navbar/Navbar';
-import {Hero} from './Components/Hero/Hero'
+import { Hero } from './Components/Hero/Hero'
 
 export const App = () => {
   let heroData = [
-    { text1: "", text2: "" },
-    { text1: "", text2: "" },
-    { text1: "", text2: "" },
+    { text1: "Indulge in", text2: "pure happiness" },
+    { text1: "Drive Green", text2: "Stay clean" },
+    { text1: "Experience the", text2: "true luxury" },
   ]
   const [heroCount, setHeroCount] = useState(2);
   const [playStatus, setPlaystatus] = useState(false);
@@ -16,14 +16,14 @@ export const App = () => {
       <Background playStatus={playStatus} heroCount={heroCount} />
       <Navbar setHeroCount={setHeroCount} setPlaystatus={setPlaystatus} ></Navbar>
       <Hero
-      setPlaystatus ={setPlaystatus}
-      setHeroCount = {setHeroCount}
-      heroCount = {heroCount}
-      playStatus={playStatus}
-      heroData={heroData[heroCount]}
-      
+        setPlaystatus={setPlaystatus}
+        setHeroCount={setHeroCount}
+        heroCount={heroCount}
+        playStatus={playStatus}
+        heroData={heroData[heroCount]}
+
       />
-      
+
     </div>
 
   )
